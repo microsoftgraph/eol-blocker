35e1c576927f4b4cb3ff32e736c4280566a28ac0
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-07-28 16:31:28 -0400

    Changed error file array to string array

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ async function run(): Promise<void> {
         // Check the contents for CRLF
         if (/\r\n/g.test(content)) {
           // Found, add to list of "bad" files
-          errorFiles.push(file);
+          errorFiles.push(file.filename);
           console.log(`File: ${file.filename} - contains CRLF`);
         } else {
           console.log(`File: ${file.filename} - no CRLF`);
@@ -47,7 +47,7 @@ async function run(): Promise<void> {
 
         // Create a bulleted list of the files
         errorFiles.forEach(file => {
-          fileList = fileList + `- ${file.filename}\n`;
+          fileList = fileList + `- ${file}\n`;
         });
 
         const prComment = `${UserStrings.PR_REPORT_HEADER}
